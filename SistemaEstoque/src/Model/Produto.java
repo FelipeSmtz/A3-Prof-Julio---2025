80cab0b60e7b465efd90f9ec25875682d752466c
Jefherson Luiz <jefhersonluiz08@gmail.com>
2025-11-19 21:15:15 -0300

    att produto.java

--- SistemaEstoque/src/Model/Produto.java
+++ SistemaEstoque/src/Model/Produto.java
@@ -6,21 +6,20 @@ import java.sql.SQLException;
 
 public class Produto {
 
-    // Atributos conforme o PDF da A3
+    // atributos
     private int id;
     private String nome;
     private String descricao;
     private int quantidade;
     private double preco;
-    private String dataCadastro; // Vamos tratar como String para facilitar a visualização na tabela
-    private final ProdutoDAO dao;
+    private final ProdutoDAO dao; // objetoDAO para conexão
 
-    // Construtor Vazio
+    // método construtor vazio
     public Produto() {
-        this.dao = new ProdutoDAO();
+        this.dao = new ProdutoDAO(); // inicializa o DAO
     }
 
-    // Construtor com dados
+    // método construtor com dados
     public Produto(int id, String nome, String descricao, int quantidade, double preco) {
         this.id = id;
         this.nome = nome;
@@ -30,7 +29,7 @@ public class Produto {
         this.dao = new ProdutoDAO();
     }
 
-    // Getters e Setters
+    // métodos get e set
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
 
@@ -45,39 +44,47 @@ public class Produto {
 
     public double getPreco() { return preco; }
     public void setPreco(double preco) { this.preco = preco; }
-    
-    public String getDataCadastro() { return dataCadastro; }
-    public void setDataCadastro(String dataCadastro) { this.dataCadastro = dataCadastro; }
 
     @Override
     public String toString() {
-        return "Produto: " + nome + " | Qtd: " + quantidade + " | R$ " + preco;
+        return "ID: " + this.id + " | Nome: " + this.nome + " | Preço: R$ " + this.preco;
     }
 
-    /* MÉTODOS PARA O DAO (Conectando com o Banco) */
-    
+    // métodos de persistência (conectando com a camada DAO)
+
+    // retorna a lista de produtos do banco
     public ArrayList<Produto> getMinhaLista() {
         return dao.getMinhaLista();
     }
 
+    // cadastra novo produto
     public boolean InsertProdutoBD(String nome, String descricao, int quantidade, double preco) throws SQLException {
-        Produto objeto = new Produto(0, nome, descricao, quantidade, preco); // ID 0 pois é auto-increment
+        int id = this.maiorID() + 1; // lógica para incrementar id ou deixar o banco cuidar disso
+        Produto objeto = new Produto(id, nome, descricao, quantidade, preco);
         dao.InsertProdutoBD(objeto);
         return true;
     }
 
+    // deleta um produto pelo id
     public boolean DeleteProdutoBD(int id) {
         dao.DeleteProdutoBD(id);
         return true;
     }
 
+    // edita um produto
     public boolean UpdateProdutoBD(int id, String nome, String descricao, int quantidade, double preco) {
         Produto objeto = new Produto(id, nome, descricao, quantidade, preco);
         dao.UpdateProdutoBD(objeto);
         return true;
     }
-    
+
+    // carrega um produto específico
     public Produto carregaProduto(int id) {
         return dao.carregaProduto(id);
+    }
+
+    // pega o maior ID para lógica manual se precisar
+    public int maiorID() throws SQLException {
+        return dao.maiorID();
     }
 }
